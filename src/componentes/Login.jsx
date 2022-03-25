17c15035e64af029d5c50d9dd5ea8b6fcd37c77f
201269ImanolMG <201269@ids.upchiapas.edu.mx>
2022-03-25 04:41:31 -0600

    Login Funcional

--- src/componentes/Login.jsx
+++ src/componentes/Login.jsx
@@ -1,51 +1,25 @@
+import React, {useEffect, useState} from 'react';
 import '../css/login.css';
-
+import axios from 'axios';
 export default function Login(props) {
     const [password,setPassword] = useState('');
     const [userName,setUsername] = useState('');
     const [redirect,setRedirect]= useState(false);
 
-    const [tokens,setTokens] = useState('');
-    const [user,setUser] = useState('');
-    const [pass,setPass] = useState(false)
-    const [error,setError] = useState("")
-    //http://localhost:8000/api/v1/login/
-
-    const verPassword = () =>{
-        if (pass == false){
-            setPass(true)
-        }else{
-            setPass(false)
-        }
-        console.log(pass)
-    }
 
     const login = async (e) => {
-        let response = await fetch('http://localhost:8000/api/v2/Auth2/',{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({'username':userName, 'password':password}),
-        })
-        let data = await response.json()
-
-
-        if (response.status === 200){
-            setUser(data)
-            setUserProfile(jwt_decode(data.access))
-            localStorage.setItem('token', data.access);
-            setRedirect(true)
-            setUserLogin(true)
-        }else{
-            //alert('algo salió mal')
-            setError("Usuario o contraseña, icorrectos")
-            setTimeout(() => {
-                setError("")
-            }, 5000);
+        try {
+            const res = await axios.post(
+                "http://localhost:5000/profile/login",JSON.stringify({'username':userName, 'password':password}),
+                {headers: {
+                        'Content-Type': 'application/json'
+                    },}
+            );
+            console.log(res);
+        } catch (ex) {
+            console.log(ex);
         }
-
-    }
+    };
 
     return (
         <div className="login">
@@ -58,16 +32,16 @@ export default function Login(props) {
                         <div className="card-body">
                             <div className="input-group mb-3">
                                 <span className="input-group-text" id="basic-addon1">👤</span>
-                                <input type="email" className="form-control" placeholder="Ingrese su nombre de usuario" aria-label="Username" aria-describedby="basic-addon2" />
+                                <input type="email" className="form-control" placeholder="Ingrese su nombre de usuario" aria-label="Username" aria-describedby="basic-addon2" onChange={(e)=>setUsername(e.target.value)}/>
                             </div>
 
                             <div className="input-group mb-3">
                                 <span className="input-group-text" id="basic-addon1">🔒</span>
-                                <input type="password" className="form-control" placeholder="Ingrese su contraseña" aria-label="clave" aria-describedby="basic-addon2" />
+                                <input type="password" className="form-control" placeholder="Ingrese su contraseña" aria-label="clave" aria-describedby="basic-addon2" onChange={(e)=>setPassword(e.target.value)}  />
                             </div>
                             <div class="d-grid gap-2">
                     
-                                <button class="btn btn-primary" type="button">Iniciar sesión</button>
+                                <button class="btn btn-primary" type="button" onClick={login}>Iniciar sesión</button>
                                 <div className="card-footer">
                                     <span>¿Olvido su contraseña?</span> <a href="#">Recuperar</a>
                                 </div>
