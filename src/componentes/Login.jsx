5af745729139d5c2c29968d4fb7b9199e2238f8b
201269ImanolMG <201269@ids.upchiapas.edu.mx>
2022-03-25 03:20:40 -0600

    Conexion al api para login

--- src/componentes/Login.jsx
+++ src/componentes/Login.jsx
@@ -1,7 +1,51 @@
 import '../css/login.css';
 
 export default function Login(props) {
+    const [password,setPassword] = useState('');
+    const [userName,setUsername] = useState('');
+    const [redirect,setRedirect]= useState(false);
 
+    const [tokens,setTokens] = useState('');
+    const [user,setUser] = useState('');
+    const [pass,setPass] = useState(false)
+    const [error,setError] = useState("")
+    //http://localhost:8000/api/v1/login/
+
+    const verPassword = () =>{
+        if (pass == false){
+            setPass(true)
+        }else{
+            setPass(false)
+        }
+        console.log(pass)
+    }
+
+    const login = async (e) => {
+        let response = await fetch('http://localhost:8000/api/v2/Auth2/',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({'username':userName, 'password':password}),
+        })
+        let data = await response.json()
+
+
+        if (response.status === 200){
+            setUser(data)
+            setUserProfile(jwt_decode(data.access))
+            localStorage.setItem('token', data.access);
+            setRedirect(true)
+            setUserLogin(true)
+        }else{
+            //alert('algo salió mal')
+            setError("Usuario o contraseña, icorrectos")
+            setTimeout(() => {
+                setError("")
+            }, 5000);
+        }
+
+    }
 
     return (
         <div className="login">
