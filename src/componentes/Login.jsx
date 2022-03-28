579d047bc84e5764364c6bd73850f11b157acecf
201269ImanolMG <201269@ids.upchiapas.edu.mx>
2022-03-28 16:13:46 -0600

    login funcional

--- src/componentes/Login.jsx
+++ src/componentes/Login.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import '../css/login.css';
 import axios from 'axios';
 export default function Login(props) {
