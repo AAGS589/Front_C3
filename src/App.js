e466f1e5a7afaa05b44093725509a7934cbf0a73
201269ImanolMG <201269@ids.upchiapas.edu.mx>
2022-03-29 22:58:09 -0600

    cambios en app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import Login from './componentes/Login';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 function App() {
   return (
-    
-    <Login />
+      <Router>
+        <Switch>
+          <Route path='/'>
+            <Login />
+          </Route>
+        </Switch>
+
+      </Router>
+
   );
 }
 
